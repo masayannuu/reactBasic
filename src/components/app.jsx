07952c916dc6762e0c3440304853c77c5246adaa
masayannuu <masayannuu@gmail.com>
2018-02-14 00:05:10 +0900

    add props & state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
+import Greeting from './greeting';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'John',
+    };
+  }
+
+  handleNameChange(name) {
+    this.setState({ name });
+  }
+
   render() {
-    return (<div>Hello from class App</div>);
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleNameChange(e.target.value)}
+        />
+        <Greeting name={this.state.name} />
+      </div>
+    );
   }
 }
 
