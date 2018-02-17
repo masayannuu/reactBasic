d547e6638b7499f3922b19020e3a21506ad77a94
masayannuu <masayannuu@gmail.com>
2018-02-17 23:57:02 +0900

    add new components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-import Greeting from './greeting';
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'John',
     };
+  }
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
   }
 
   handleNameChange(name) {
     this.setState({ name });
   }
 
+  handlePlaceSubmit(place) {
+    axios
+			.get(GEOCODE_ENDPOINT, { params: { address:place } })
+			.then((results) => {
+        const data = results.data;
+        const result = results.data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('データが見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        this.setErrorMessage('通信に失敗しました');
+      })
+  }
+
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}
+        <h1>経度緯度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
         />
-        <Greeting name={this.state.name} />
+        <Map lat={this.state.lat} lng={this.state.lng} />
       </div>
     );
   }
