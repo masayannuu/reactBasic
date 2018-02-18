bf8cbb5efb924c1a618adfe6cefd2118b07bdd60
masayannuu <masayannuu@gmail.com>
2018-02-18 22:52:39 +0900

    add components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,76 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-import SearchForm from './SearchForm';
-import GeocodeResult from './GeocodeResult';
-import Map from './Map';
+import SearchPage from './SearchPage';
 
-const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  setErrorMessage(message) {
-    this.setState({
-      address: message,
-      lat: 0,
-      lng: 0,
-    });
-  }
-
-  handleNameChange(name) {
-    this.setState({ name });
-  }
-
-  handlePlaceSubmit(place) {
-    axios
-			.get(GEOCODE_ENDPOINT, { params: { address:place } })
-			.then((results) => {
-        const data = results.data;
-        const result = results.data.results[0];
-        switch (data.status) {
-          case 'OK': {
-            const location = result.geometry.location;
-            this.setState({
-              address: result.formatted_address,
-              lat: location.lat,
-              lng: location.lng,
-            });
-            break;
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('データが見つかりませんでした');
-            break;
-          }
-          default: {
-            this.setErrorMessage('エラーが発生しました');
-          }
-        }
-      })
-      .catch((e) => {
-        console.log(e);
-        this.setErrorMessage('通信に失敗しました');
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>経度緯度検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
-        <GeocodeResult
-          address={this.state.address}
-          lat={this.state.lat}
-          lng={this.state.lng}
-        />
-        <Map lat={this.state.lat} lng={this.state.lng} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <ul className="left-navi">
+      <li><a href="/">ホテル検索</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
+    <SearchPage />
+  </div>
+);
 
 export default App;
