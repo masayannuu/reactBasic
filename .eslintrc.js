608c1acf1a1411993beca458458bf2e8dab23e4c
masayannuu <masayannuu@gmail.com>
2018-03-05 23:40:45 +0900

    add redux-dev-tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,12 @@ module.exports = {
   env: {
     browser: true,
   },
+  rules: {
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['__REDUX_DEVTOOLS_EXTENSION__'] },
+    ],
+  },
   plugins: [
     'react',
     'jsx-a11y',
